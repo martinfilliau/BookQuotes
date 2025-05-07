08d33e83de08887938ad834535398fe18b3e3801
Martin Filliau <martin@filliau.com>
2025-05-07 20:53:51 +0200

    refactor: improve XML parsing

--- BookQuotes.Infrastructure/Parsers/XmlAnnotationsParser.cs
+++ BookQuotes.Infrastructure/Parsers/XmlAnnotationsParser.cs
@@ -1,3 +1,4 @@
+using System.Xml;
 using System.Xml.Linq;
 using BookQuotes.Domain.Entities;
 
@@ -5,60 +6,67 @@ namespace BookQuotes.Infrastructure.Parsers;
 
 public static class XmlAnnotationsParser
 {
-    static XNamespace dc = "http://purl.org/dc/elements/1.1/";
-    static XNamespace ns = "http://ns.adobe.com/digitaleditions/annotations";
+    private static readonly XNamespace Ns = "http://ns.adobe.com/digitaleditions/annotations";
+    private static readonly XNamespace Dc = "http://purl.org/dc/elements/1.1/";
 
     public static Book? Parse(string xmlData)
     {
         if (string.IsNullOrWhiteSpace(xmlData))
             return null;
-        
-        var bookTitle = "";
-        var author = "";
-        var quotes = new List<Quote>();
-        
+
         var doc = XDocument.Parse(xmlData);
-
-        // Extract publication details
-        XElement? publication = doc?.Root?.Element(ns + "publication");
-        
-        if (publication == null)
-            throw new Exception("Unable to parse publication");
-        
-        bookTitle = publication.Element(dc + "title")?.Value;
-        author = publication.Element(dc + "creator")?.Value;
-
-        // Extract annotations
-        var annotations = doc?.Root?.Elements(ns + "annotation");
-        
-        if (annotations == null)
-            return new Book
-            {
-                Title = bookTitle,
-                Author = author,
-                Quotes = []
-            };
-        
-        foreach (var annotation in annotations)
-        {
-            var fragment = annotation.Element(ns + "target")?.Element(ns + "fragment");
-            
-            if (fragment == null) continue;
-            
-            var comment = fragment.Element(ns + "text")?.Value ?? "";
-                
-            quotes.Add(new Quote
-            {
-                Comment = ValueCleaner.CleanupValue(comment),
-                Position = fragment.Attribute("start")?.Value ?? "",
-            });
-        }
+        var publication = ExtractPublication(doc);
+        var (title, author) = ExtractBookDetails(publication);
+        var quotes = ExtractQuotes(doc);
 
         return new Book
         {
-            Title = bookTitle,
+            Title = title,
             Author = author,
-            Quotes = quotes,
+            Quotes = quotes
+        };
+    }
+
+    private static XElement ExtractPublication(XDocument? doc)
+    {
+        return doc?.Root?.Element(Ns + "publication")
+               ?? throw new Exception("Unable to parse publication: missing publication element");
+    }
+
+    private static (string title, string author) ExtractBookDetails(XElement publication)
+    {
+        var title = publication.Element(Dc + "title")?.Value
+                    ?? throw new Exception("Unable to parse publication: missing title");
+        var author = publication.Element(Dc + "creator")?.Value
+                     ?? throw new Exception("Unable to parse publication: missing author");
+
+        return (title, author);
+    }
+
+    private static List<Quote> ExtractQuotes(XDocument? doc)
+    {
+        var quotes = new List<Quote>();
+        var annotations = doc?.Root?.Elements(Ns + "annotation");
+
+        if (annotations is null)
+            return [];
+
+        foreach (var annotation in annotations)
+            if (TryExtractQuote(annotation) is { } quote)
+                quotes.Add(quote);
+
+        return quotes;
+    }
+
+    private static Quote? TryExtractQuote(XElement annotation)
+    {
+        var fragment = annotation.Element(Ns + "target")?.Element(Ns + "fragment");
+        if (fragment is null) return null;
+
+        return new Quote
+        {
+            Comment = ValueCleaner.CleanupValue(fragment.Element(Ns + "text")?.Value ?? ""),
+            Position = fragment.Attribute("start")?.Value ?? ""
         };
     }
 }
