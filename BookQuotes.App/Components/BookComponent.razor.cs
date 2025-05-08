6d29a3b3b5fd75fa62ec1604a83a5270adbaf483
Martin Filliau <martin@filliau.com>
2025-05-08 21:16:11 +0200

    refactor: upload multiple files, and display them accordingly

--- BookQuotes.App/Components/BookComponent.razor.cs
+++ BookQuotes.App/Components/BookComponent.razor.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using BookQuotes.App.Services;
+using BookQuotes.Application.Commands;
 using BookQuotes.Domain.Entities;
 using Microsoft.AspNetCore.Components;
 
@@ -7,8 +10,18 @@ public partial class BookComponent : ComponentBase
 {
     [Parameter] public Book Book { get; set; }
 
+    [Inject] IExportQuotes ExportQuotes { get; set; }
+    [Inject] FileDownloaderService FileDownloaderService { get; set; }
+    
     List<string> TocItems =>
         Book?.TableOfContents == null
             ? []
             : Book.TableOfContents.GetTableOfContentsAsArray();
+
+    private async Task ExportQuotesMarkdown()
+    {
+        var markdown = ExportQuotes.ExportQuotesToMarkdown(Book, Book.Quotes);
+        using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(markdown));
+        await FileDownloaderService.DownloadFileAsync($"{Book.Title} - quotes.md", memoryStream, "text/plain");
+    }
 }
--- BookQuotes.App/Components/UploadComponent.razor.cs
+++ BookQuotes.App/Components/UploadComponent.razor.cs
@@ -1,5 +1,3 @@
-using System.Text;
-using BookQuotes.App.Services;
 using BookQuotes.Application.Commands;
 using BookQuotes.Domain.Entities;
 using Microsoft.AspNetCore.Components;
@@ -10,95 +8,113 @@ namespace BookQuotes.App.Components;
 
 public partial class UploadComponent : ComponentBase
 {
-    [Inject] IDialogService DialogService { get; set; }
-    [Inject] IAnalyseEpubFile AnalyseEpubFile { get; set; }
-    [Inject] IAnalyseQuotes AnalyseQuotes { get; set; }
-    [Inject] IExportQuotes ExportQuotes { get; set; }
-    [Inject] FileDownloaderService FileDownloaderService { get; set; }
-
     private const int MaxFileSize = 30000000; // 30 MB
+    [Inject] private IDialogService DialogService { get; set; }
+    [Inject] private IAnalyseEpubFile AnalyseEpubFile { get; set; }
+    [Inject] private IAnalyseQuotes AnalyseQuotes { get; set; }
 
-    private IBrowserFile? _epub;
-    private IBrowserFile? _xml;
-    private bool _canStartAnalysis = false;
+    private List<Book> Books { get; set; } = new();
 
-    private Book? _book;
-    
-    private bool CanExportQuotes => _book is not null && _book.Quotes.Count > 0;
-    
-    private async Task UploadFiles(IReadOnlyList<IBrowserFile> files)
+    private async Task UploadFiles(IReadOnlyList<IBrowserFile>? files)
     {
-        //      --> check title matches
-       
-        // Append multiple files
-        
+        if (files == null) return;
+
         foreach (var file in files)
         {
             if (IsEpubFile(file))
             {
-                _epub = file;
-                _canStartAnalysis = true;
-                break;
+                await ProcessEpubFile(file);
+                continue;
             }
 
             if (IsAnnotationsFile(file))
             {
-                _xml = file;
-                _canStartAnalysis = true;
-                break;
+                await ProcessAnnotationFile(file);
+                continue;
             }
 
             await UnknownFile(file);
         }
     }
 
-    private void Reset()
+    private async Task ProcessEpubFile(IBrowserFile file)
     {
-        _canStartAnalysis = false;
-        _epub = null;
-        _xml = null;
-        _book = null;
-    }
-
-    async Task StartAnalysis()
-    {
-        if (!_canStartAnalysis)
-            return;
-        
-        if (_epub != null)
+        try
         {
-            await using var stream = _epub.OpenReadStream(MaxFileSize);
+            await using var stream = file.OpenReadStream(MaxFileSize);
             using var ms = new MemoryStream();
             await stream.CopyToAsync(ms);
- 
+
             var result = await AnalyseEpubFile.Analyse(ms);
-            _book = result;
-        }
 
-        if (_xml != null)
+            if (result == null)
+            {
+                await ShowMessageBox("Error", $"File {file.Name} is not a valid epub file.");
+                return;           
+            }
+            
+            var index = Books.FindIndex(b => b.Title == result.Title);
+            if (index >= 0)
+            {
+                Books[index] = Books[index] with { TableOfContents = result.TableOfContents };
+            }
+            else
+            {
+                Books.Add(result);
+            }
+        }
+        catch (Exception e)
         {
-            using var reader = new StreamReader(_xml.OpenReadStream());
-            var text = await reader.ReadToEndAsync();
-            _book = AnalyseQuotes.Analyse(text);
+            await ShowMessageBox("Error", e.Message);
         }
     }
 
-    async Task ExportQuotesMarkdown()
+    private async Task ProcessAnnotationFile(IBrowserFile file)
     {
-        if (_book == null)
-            return;
-        var markdown = ExportQuotes.ExportQuotesToMarkdown(_book, _book.Quotes);
-        using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(markdown));
-        await FileDownloaderService.DownloadFileAsync($"{_book.Title} - quotes.md", memoryStream, "text/plain");
+        using var reader = new StreamReader(file.OpenReadStream());
+        var text = await reader.ReadToEndAsync();
+        var bookQuotes = AnalyseQuotes.Analyse(text);
+
+        if (bookQuotes != null)
+        {
+            var index = Books.FindIndex(b => b.Title == bookQuotes.Title);
+            if (index >= 0)
+            {
+                Books[index] = Books[index] with { Quotes = bookQuotes.Quotes };
+            }
+            else
+            {
+                Books.Add(bookQuotes);
+            }
+        }
+        else
+        {
+            await ShowMessageBox("Error", $"File {file.Name} is not a valid annotation file.");
+        }
     }
-    
-    private static bool IsEpubFile(IBrowserFile file) => file.ContentType == "application/epub+zip" || file.Name.EndsWith(".epub");
-    
-    private static bool IsAnnotationsFile(IBrowserFile file) => file.Name.EndsWith(".xml") || file.Name.EndsWith(".annot");
+
+    private void Reset()
+    {
+        Books = [];
+    }
+
+    private static bool IsEpubFile(IBrowserFile file)
+    {
+        return file.ContentType == "application/epub+zip" || file.Name.EndsWith(".epub");
+    }
+
+    private static bool IsAnnotationsFile(IBrowserFile file)
+    {
+        return file.Name.EndsWith(".xml") || file.Name.EndsWith(".annot");
+    }
 
     private async Task UnknownFile(IBrowserFile file)
     {
-        await DialogService.ShowMessageBox("Alert",
-            $"File {file.Name} is not an epub file nor an XML (annotations) file.");
+        await ShowMessageBox("Alert", $"File {file.Name} is not an epub file nor an XML (annotations) file.");
+    }
+
+    private async Task ShowMessageBox(string title, string message)
+    {
+        await DialogService.ShowMessageBox(title, message);
     }
 }
