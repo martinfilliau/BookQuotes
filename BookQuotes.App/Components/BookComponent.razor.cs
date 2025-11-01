8f1991a568b011dec2a597f8e010bbcafb321594
Martin Filliau <martin@filliau.com>
2025-11-01 14:55:39 +0100

    feat: copy markdown to clipboard + handle errors

--- BookQuotes.App/Components/BookComponent.razor.cs
+++ BookQuotes.App/Components/BookComponent.razor.cs
@@ -3,6 +3,8 @@ using BookQuotes.App.Services;
 using BookQuotes.Application.Commands;
 using BookQuotes.Domain.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
 
 namespace BookQuotes.App.Components;
 
@@ -12,6 +14,9 @@ public partial class BookComponent : ComponentBase
 
     [Inject] IExportBook ExportBook { get; set; } = null!;
     [Inject] FileDownloaderService FileDownloaderService { get; set; } = null!;
+    [Inject] IJSRuntime JSRuntime { get; set; } = null!;
+    [Inject] ISnackbar Snackbar { get; set; } = null!;
+    [Inject] IDialogService DialogService { get; set; } = null!;
 
     private TableOfContentsItem? RootToc => Book?.TableOfContents?.Root;
 
@@ -22,8 +27,56 @@ public partial class BookComponent : ComponentBase
 
     private async Task ExportQuotesMarkdown()
     {
-        var markdown = ExportBook.ExportToMarkdown(Book);
-        using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(markdown));
-        await FileDownloaderService.DownloadFileAsync($"{Book.Title}.md", memoryStream, "text/plain");
+        string? error = null;
+        var success = false;
+
+        try
+        {
+            var markdown = ExportBook.ExportToMarkdown(Book);
+            using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(markdown));
+            await FileDownloaderService.DownloadFileAsync($"{Book.Title}.md", memoryStream, "text/plain");           
+            success = true;
+        }
+        catch (Exception e)
+        {
+            error = e.Message;
+        }
+
+        if (success)
+        {
+            Snackbar.Add("File successfully downloaded", Severity.Success);
+        }
+
+        if (!string.IsNullOrWhiteSpace(error))
+        {
+            await DialogService.ShowMessageBox("Error: unable to export", error);
+        }
+    }
+
+    private async Task CopyToClipboard()
+    {
+        string? error = null;
+        var success = false;
+        
+        try
+        {
+            var markdown = ExportBook.ExportToMarkdown(Book);
+            await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", markdown);
+            success = true;
+        }
+        catch (Exception e)
+        {
+            error = e.Message;
+        }
+
+        if (success)
+        {
+            Snackbar.Add("Markdown copied to clipboard", Severity.Success);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(error))
+        {
+            await DialogService.ShowMessageBox("Error: unable to copy", error);
+        }
     }
 }
